1d80b387d286fffd4d78768263124bd489fa667b
jordynalexis006 <55261827+jordynalexis006@users.noreply.github.com>
2019-10-10 18:06:18 -0500

    Changed move system to use physics

--- Unity Project/Assets/Scripts/MovingRampSupport.cs
+++ Unity Project/Assets/Scripts/MovingRampSupport.cs
@@ -4,25 +4,22 @@ using UnityEngine;
 
 public class MovingRampSupport : MonoBehaviour
 {
-    private float moveSpeed = 0.1f;
+    public float moveSpeed = 1f;
+    private float verticalInput; 
+    private Rigidbody2D rigidbody;
+    
     // Start is called before the first frame update
     void Start()
-    {
-        Debug.Log("Test hello world");
+    { 
+        rigidbody = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    // Use fixed update for physics code, because we need to be careful about how often we call expensive, hardware intensive, physics stuff.
+    private void FixedUpdate()
     {
-        // if the player presses up arrow, we move the sqaure up.
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(0, moveSpeed, 0);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(0, -moveSpeed, 0);
-        }
+        verticalInput = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(0, verticalInput * moveSpeed);
     }
 
 }
