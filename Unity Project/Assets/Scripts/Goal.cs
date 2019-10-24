cf2121cba5629ac17701d873a7220d6228c057e2
jordynalexis006 <55261827+jordynalexis006@users.noreply.github.com>
2019-10-24 18:09:28 -0500

    We made goal trigger and added win text.

---
+++ Unity Project/Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Detects when the ball touches the goal,, and then wins the level.
+/// </summary>
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched trigger.");
+        if (collision.tag == "Player")
+        {
+            Debug.Log("The player has entered the goal");
+        }
+    }
+}
