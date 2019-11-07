74ee139e17bf298cf8ca006cbe6f9996f882e411
jordynalexis006 <55261827+jordynalexis006@users.noreply.github.com>
2019-11-07 17:55:51 -0600

    added goal sfx and music

--- Unity Project/Assets/Scripts/Goal.cs
+++ Unity Project/Assets/Scripts/Goal.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 /// Detects when the ball touches the goal,, and then wins the level.
 /// </summary>
 public class Goal : MonoBehaviour
+
 {
+    public GameObject winText;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Touched trigger.");
         if (collision.tag == "Player")
         {
+            winText.SetActive(true);
+            // cue sound effect here
+            audioSource.Play();
             Debug.Log("The player has entered the goal");
         }
     }
